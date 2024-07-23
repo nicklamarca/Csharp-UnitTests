3e74367f4d87f60af937e046e5bd033e720e30e5
Nick Lamarca <nicklamarca64@gmail.com>
2024-07-23 16:20:12 -0500

    Add project files.

---
+++ UnitTests/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,28 @@
+﻿
+namespace UnitTests.Tests
+{
+    public static class WorldsDumbestFunctionTests
+    {
+        //Naming Convention - ClassName_MethodName_ExpectedResult
+        public static void WorldsDumbestFunction_ReturnsDumbThingsIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange - Get your variables, classes, functions
+                int num = 0;
+                WorldsDumbestFunction dumbFunction = new WorldsDumbestFunction();
+
+                //Act - Execute the function
+                string result = dumbFunction.ReturnsDumbThingsIfZero(num);
+
+                //Assert - What should the result be
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ UnitTests/WorldsDumbestFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class WorldsDumbestFunction
+    {
+        public string ReturnsDumbThingsIfZero(int num)
+        {
+            if(num == 0)
+            {
+                return "This Is Dumb";
+            }
+            else
+            {
+                return "Great Code";
+            }             
+        }
+    }
+}
