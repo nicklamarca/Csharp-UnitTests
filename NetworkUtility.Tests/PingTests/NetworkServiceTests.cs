33dea5f92d722a6dada75d72b41930513d2ff971
Nick Lamarca <nicklamarca64@gmail.com>
2024-07-23 17:58:59 -0500

    Add Fluent Assertion Unit Tests (Simple)

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,42 @@
+﻿
+using NetworkUtility.Ping;
+using FluentAssertions;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            var networkService = new NetworkService();
+            
+            // Act
+            var result = networkService.SendPing();
+            
+            // Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(6,4,10)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var networkService = new NetworkService();
+            
+            // Act
+            var result = networkService.PingTimeout(a, b);
+            
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(3);
+            result.Should().BeInRange(0,100);
+
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,19 @@
+﻿
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            //SearchDNS()
+            //BuildPacket()
+            return "Success: Ping Sent!";
+        }
+
+        public int PingTimeout(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
